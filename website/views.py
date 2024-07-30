ef88d6b39a8c9b449015972a2f6f7475ef7e3baf
Gabriel DM <gnizto@outlook.com>
2024-07-29 22:02:12 -0300

    Add login and logout

--- website/views.py
+++ website/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            messages.success(request, "You've been logged in!")
+        else:
+            messages.error(request, "Credentials does not match!")
+    return render(request, 'home.html', {})
+
+def login_user(request):
+    pass
+
+def register_user(request):
+    return render(request, 'register.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You've been logged out!")
     return render(request, 'home.html', {})
